073c936f82dc7498be669179fc63443f56a43faf
Paulius Kuzmarskis <likutis2009@gmail.com>
2024-12-13 15:13:16 +0100

    New Combinational Logic
    COMBINE ALL

--- main_drummer/drummer.sv
+++ main_drummer/drummer.sv
@@ -1,81 +1,169 @@
-module counter2(SW, HEX0, KEY);
-input [9:0] SW;
-input [0:1] KEY;
-output [0:6] HEX0;
-wire UD, Re, clk;
-wire [3:0] num;
-wire [1:0] display;
-assign clk = KEY[1];
-assign UD = SW[0];
-assign Re = ~KEY[0];
-count(UD, Re, clk, num);
-display(num, HEX0);
+module (input [9:0] SW , MAX10_CLK1_50, input GPIO [35:0]);
+
+sine_gen sine(SW[9:0], GPIO[35:0], MAX10_CLK1_50)
+
 endmodule
 
-module count(UD, Re, clk, q);
-input UD, Re, clk;
-output [3:0] q;
+// Modules for -----Noise_gen____
+module noise_gen (in, out, MAX10_CLK1_50);
+    input [7:0] in;
+    input MAX10_CLK1_50;
+    output reg [7:0] out;
+	 
+	wire clk;
+   wire [4:0] noise;
 
+	assign clk = MAX10_CLK1_50;
+	
+   random ran1(clk, noise, reset);
+    
+    always_comb
+    begin
+    if(noise[0])
+        if(in + noise > 9'b111111111)
+            out = 8'b11111111;
+        else 
+            out = in + noise;
+    
+    else if(in > noise)
+        out = in - noise;
+    
+    else 
+        out = 8'b00000000;
 
+    end
 
-typedef enum logic [3:0] {s0, s1, s2, s3, s4, s5, s6, s7, s8, s9} statetype;
-statetype state, nextstate;
-always_ff @ (posedge clk, posedge Re)
-begin
-if(Re) state <= s0;
-else state <= nextstate;
-end
-always_comb
-if(UD)
-begin
-case(state)
-s0: nextstate = s1;
-s1: nextstate = s2;
-s2: nextstate = s3;
-s3: nextstate = s4;
-s4: nextstate = s5;
-s5: nextstate = s6;
-s6: nextstate = s7;
-s7: nextstate = s8;
-s8: nextstate = s9;
-s9: nextstate = s0;
-default: nextstate = s0;
-endcase
-end
-else
-begin
-case(state)
-s0: nextstate = s9;
-s1: nextstate = s0;
-s2: nextstate = s1;
-s3: nextstate = s2;
-s4: nextstate = s3;
-s5: nextstate = s4;
-s6: nextstate = s5;
-s7: nextstate = s6;
-s8: nextstate = s7;
-s9: nextstate = s8;
-default: nextstate = s0;
-endcase
-end
-assign q = state;
 endmodule
 
-module display(num, HEX);
-input [3:0] num;
-output [6:0] HEX;
-always_comb
-case(num)
-4'b0000: HEX = 7'b0000001;
-4'b0001: HEX = 7'b1001111;
-4'b0010: HEX = 7'b0010010;
-4'b0011: HEX = 7'b0000110;
-4'b0100: HEX = 7'b1001100;
-4'b0101: HEX = 7'b0100100;
-4'b0110: HEX = 7'b0100000;
-4'b0111: HEX = 7'b0001111;
-4'b1000: HEX = 7'b0000000;
-4'b1001: HEX = 7'b0000100;
-default: HEX = 7'b0000001;
-endcase 
+
+//  module that produces a five bit pseudo random value using a 5 bit register and an xor function
+//  Can be asyncronously reset using the reset input
+
+module random #(parameter WIDTH = 5)
+(clk, q, reset);
+	input clk, reset;
+	output q;
+
+	reg [WIDTH - 1:0] q;
+
+	always @(posedge clk or posedge reset)
+		begin
+			if(reset) 
+            begin
+				q[0] <= 1;
+                q[1] <= 1;
+                q[2] <= 0;
+                q[3] <= 0;
+                q[4] <= 1;
+            end
+            else 
+				q[0] <= q[WIDTH -1] ^ q[WIDTH - 3];
+				for(int i = 1; i <= WIDTH-1; i++) q[i] <= q[i-1];
+ 		end
 endmodule
+//-----Noise_gen------
+
+
+
+
+//------sine_gen------
+module sine_gen(
+	input [9:0] SW ,
+	output [35:0] GPIO,
+	input MAX10_CLK1_50);
+	
+	reg [7:0] out;
+   reg [7:0] state;
+	wire clk;
+	wire reset;
+	
+	assign reset = SW[0];
+    
+   clkDivider clkM (MAX10_CLK1_50, reset, clk);
+	 
+	 always @ (posedge clk or posedge reset)
+        begin
+        if(reset) 
+				state <= 8'b0;
+		  else if (state == 8'b00011111) 
+            state <= 8'b0;
+        else
+            state <= state + 1;
+        end
+
+    always @ (posedge clk)
+        begin
+            case(state)
+                8'h00 : out = 8'h80;
+                8'h01 : out = 8'h98;
+                8'h02 : out = 8'hB0;
+                8'h03 : out = 8'hC7;
+                8'h04 : out = 8'hDA;
+                8'h05 : out = 8'hEA;
+                8'h06 : out = 8'hF6;
+                8'h07 : out = 8'hFD;
+                8'h08 : out = 8'hFF;
+                8'h09 : out = 8'hFB;
+                8'h0a : out = 8'hF6;
+                8'h0b : out = 8'hEA;
+                8'h0c : out = 8'hDA;
+                8'h0d : out = 8'hC7;
+                8'h0e : out = 8'hB0;
+                8'h0f : out = 8'h98;
+                8'h10 : out = 8'h80;
+                8'h11 : out = 8'h67;
+                8'h12 : out = 8'h4F;
+                8'h13 : out = 8'h38;
+                8'h14 : out = 8'h25;
+                8'h15 : out = 8'h15;
+                8'h16 : out = 8'h9;
+                8'h17 : out = 8'h2;
+                8'h18 : out = 8'h0;
+                8'h19 : out = 8'h2;
+                8'h1a : out = 8'h9;
+                8'h1b : out = 8'h15;
+                8'h1c : out = 8'h25;
+                8'h1d : out = 8'h38;
+                8'h1e : out = 8'h4F;
+                8'h1f : out = 8'h67;
+                default : out  = 8'h80;
+            endcase
+        end
+
+    assign GPIO [7:0] = out;
+endmodule
+
+
+
+
+// It seems like the internal clk we use is running at 1.55 MHz 
+// Remember that freq = 2 x stop
+
+module clkDivider(clk, reset, out);
+    input clk, reset;
+    output out;
+	
+reg [31:0] count;
+reg [31:0] stop = 32'hF1b85;
+reg outReg;
+
+always @(posedge clk or posedge reset)
+    begin
+        if(reset)
+        begin
+            count <= 32'b0;
+            outReg <= 0;
+         end
+
+        else if(count == stop)
+            begin
+            count <= 32'b0;
+            outReg <= ~outReg;
+            end
+        else
+            count <= count + 1;
+    end
+
+assign out = outReg;
+endmodule
+//----sine_gen-----
